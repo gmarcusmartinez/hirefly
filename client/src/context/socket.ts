33a6ecbdea0edaf05af5c50e6ec15526fb2e900c
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-07-19 14:49:48 +0200

    update socket var for cline

--- client/src/context/socket.ts
+++ client/src/context/socket.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:5000');
+// export const socket = io('http://localhost:5000');
+export const socket = io('https://hirefly-mvp.herokuapp.com/');
 export const SocketContext = React.createContext(socket);
