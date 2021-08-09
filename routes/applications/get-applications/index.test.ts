7415c4f851c5d9244c77bcabd4cbab61ebf1b911
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-08-09 14:49:27 +0200

    fix get-applications and delete-job test suites to account for new job model requirements

--- routes/applications/get-applications/index.test.ts
+++ routes/applications/get-applications/index.test.ts
@@ -1,17 +1,15 @@
 import request from 'supertest';
 import { app } from '../../../app';
-import { fakeAuthCookie } from '../../../test/auth-helper';
 
 describe('Route Access', () => {
   it('has a route handler listening to /api/applications for post requests', async () => {
-    const response = await request(app).get('/api/applications').send({});
+    const response = await request(app).get('/api/applications/:id').send({});
     expect(response.status).not.toEqual(404);
   });
 
   it('can only be accessed if a user is signed in', async () => {
     const response = await request(app)
-      .get('/api/applications')
-      .send({})
+      .get('/api/applications/:id')
       .expect(401);
     const errMsg = 'Not Authorized';
     expect(response.body.errors[0].message).toBe(errMsg);
